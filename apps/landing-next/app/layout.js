5c8ac284aea2f7b5d35de027340fae419876cb0f
MiguelRosas11 <roguemijose@gmail.com>
2026-05-25 16:13:28 -0600

    Suppress body hydration warning

--- apps/landing-next/app/layout.js
+++ apps/landing-next/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>
+      <body suppressHydrationWarning>
         <Navbar />
         <main>{children}</main>
         <Footer />
